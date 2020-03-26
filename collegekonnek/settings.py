b5656810cad5c160a9ae4376f00ad492388a3e3f
josueadimado <josueadimado@gmail.com>
2020-03-26 02:14:36 +0000

    superuser added

--- collegekonnek/settings.py
+++ collegekonnek/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h)f5(c1%a%hxjtli=z)zq_7w2f651*e@jsjs=ceywn7wdea64g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.63.92.65']
 
 
 # Application definition
