621d231192f23bb1b2b5660dc17adeacb150a21e
josueadimado <josueadimado@gmail.com>
2020-03-29 00:45:01 +0000

    commission

--- static/assets/js/allquestions.js
+++ static/assets/js/allquestions.js
@@ -3,43 +3,47 @@ var questions = [
     'Analyze data using statistics','Counsel people with drug or addiction','Create art for sale and exhibition'
 ]
 
-
-
+    function results(){
+        window.open("https://vvu.edu.gh")
+    };
 
 $('#submit').click(function() {
-    $('#question').html(questions.pop());
-    $('#options').html(`<!--Material Radio-->
-                                                <label class="material-radio is-accent">
-                                                    <input type="radio" name="group-demo1" >
-                                                    <span class="dot"></span>
-                                                    <span class="radio-label">Hate it</span>
-                                                </label>
-                                                <!--Material Radio-->
-                                                <label class="material-radio is-accent">
-                                                    <input type="radio" name="group-demo1">
-                                                    <span class="dot"></span>
-                                                    <span class="radio-label">Dislike it</span>
-                                                </label>
-                                                <!--Material Radio-->
-                                                <label class="material-radio is-accent">
-                                                    <input type="radio" name="group-demo1">
-                                                    <span class="dot"></span>
-                                                    <span class="radio-label">Neutral</span>
-                                                </label>
-                                                <!--Material Radio-->
-                                                <label class="material-radio is-accent">
-                                                    <input type="radio" name="group-demo1">
-                                                    <span class="dot"></span>
-                                                    <span class="radio-label">Like it</span>
-                                                </label>
-                                                <!--Material Radio-->
-                                                <label class="material-radio is-accent">
-                                                    <input type="radio" name="group-demo1">
-                                                    <span class="dot"></span>
-                                                    <span class="radio-label">Love it</span>
-                                                </label>`);
+    for(var i = 0;i< questions.length;i++){
+        $('#question').html(questions.pop());
+        $('#options').html(`<!--Material Radio-->
+                                                    <label class="material-radio is-accent">
+                                                        <input type="radio" name="group-demo1" >
+                                                        <span class="dot"></span>
+                                                        <span class="radio-label">Hate it</span>
+                                                    </label>
+                                                    <!--Material Radio-->
+                                                    <label class="material-radio is-accent">
+                                                        <input type="radio" name="group-demo1">
+                                                        <span class="dot"></span>
+                                                        <span class="radio-label">Dislike it</span>
+                                                    </label>
+                                                    <!--Material Radio-->
+                                                    <label class="material-radio is-accent">
+                                                        <input type="radio" name="group-demo1">
+                                                        <span class="dot"></span>
+                                                        <span class="radio-label">Neutral</span>
+                                                    </label>
+                                                    <!--Material Radio-->
+                                                    <label class="material-radio is-accent">
+                                                        <input type="radio" name="group-demo1">
+                                                        <span class="dot"></span>
+                                                        <span class="radio-label">Like it</span>
+                                                    </label>
+                                                    <!--Material Radio-->
+                                                    <label class="material-radio is-accent">
+                                                        <input type="radio" name="group-demo1">
+                                                        <span class="dot"></span>
+                                                        <span class="radio-label">Love it</span>
+                                                    </label>`);
 
-    
+    }
+
+    results();
+   
+   
 });
-
-$('#submit').show
